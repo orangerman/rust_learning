13a4e7681b5e53d96a7c10414722355a73ab8e7a
Java_Fan <1057600474@qq.com>
2025-07-27 12:23:38 +0800

    query data

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,10 @@ mod entity;
 mod logger;
 
 use anyhow::Ok;
-use axum::{Router, debug_handler, routing};
+use axum::{Router, debug_handler, extract::State, response::IntoResponse, routing};
+use entity::prelude::*;
+use entity::*;
+use sea_orm::prelude::*;
 use tokio::net::TcpListener;
 
 #[tokio::main]
@@ -13,14 +16,17 @@ async fn main() -> anyhow::Result<()> {
     logger::init();
 
     // 数据库初始化
-    database::init().await?;
+    let db = database::init().await?;
 
     //路由配置
-    let router: Router = Router::new().route("/", routing::get(index));
+    let router: Router = Router::new()
+        .route("/", routing::get(index))
+        .route("/users", routing::get(find_user))
+        .with_state(db);
 
     let port: u16 = config::get().server().port();
     // 监听配置
-    let bind_add: String = format!("0.0.0.0:{port}");
+    let bind_add: String = format!("127.0.0.1:{port}");
     let listener: TcpListener = TcpListener::bind(&bind_add).await.unwrap();
 
     tracing::info!("Listener bind {bind_add}");
@@ -32,4 +38,14 @@ async fn main() -> anyhow::Result<()> {
 #[debug_handler]
 async fn index() -> &'static str {
     "hello rust!"
+}
+
+#[debug_handler]
+async fn find_user(State(db): State<DatabaseConnection>) -> impl IntoResponse {
+    let users = User::find()
+        .filter(user::Column::Name.eq("123123"))
+        .all(&db)
+        .await
+        .unwrap();
+    axum::Json(users)
 }
