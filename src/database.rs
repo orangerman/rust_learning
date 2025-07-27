11d2e0a169b909ce9769b5424f000d5b80c1527a
fanzq <fzq1057600474@gamil.com>
2025-07-27 10:46:46 +0800

    database support

--- src/database.rs
+++ src/database.rs
@@ -1,10 +1,15 @@
+use std::time::Duration;
+
 use super::config;
-use sea_orm::{ConnectOptions, DatabaseConnection};
+use anyhow::Ok;
+use sea_orm::{
+    ConnectOptions, ConnectionTrait, Database, DatabaseConnection, DbBackend, Statement,
+};
 
 pub async fn init() -> anyhow::Result<DatabaseConnection> {
     let database_config: &config::DataBaseConfig = config::get().database();
 
-    let options = ConnectOptions::new(format!(
+    let mut options: ConnectOptions = ConnectOptions::new(format!(
         "mysql://{}:{}@{}:{}/{}",
         database_config.username(),
         database_config.password(),
@@ -13,5 +18,33 @@ pub async fn init() -> anyhow::Result<DatabaseConnection> {
         database_config.database()
     ));
 
-    todo!()
+    options
+        .min_connections(2)
+        .max_connections(50)
+        .connect_timeout(Duration::from_secs(10))
+        .acquire_timeout(Duration::from_secs(10))
+        .idle_timeout(Duration::from_secs(60))
+        .max_lifetime(Duration::from_secs(1800))
+        .sqlx_logging(false);
+    let db: DatabaseConnection = Database::connect(options).await?;
+    db.ping().await?;
+    tracing::info!("Databse connected successfully");
+
+    log_database_version(&db).await?;
+
+    Ok(db)
+}
+
+async fn log_database_version(db: &DatabaseConnection) -> anyhow::Result<()> {
+    let version_result = db
+        .query_one(Statement::from_string(DbBackend::MySql, "select version()"))
+        .await?
+        .ok_or_else(|| anyhow::anyhow!("Database init failed"))?;
+
+    tracing::info!(
+        "Database version is {}",
+        version_result.try_get_by_index::<String>(0)?
+    );
+
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,30 @@ mod config;
 mod database;
 mod logger;
 
+use anyhow::Ok;
 use axum::{Router, debug_handler, routing};
 use tokio::net::TcpListener;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> anyhow::Result<()> {
     // 日志初始化
     logger::init();
+
+    // 数据库初始化
+    database::init().await?;
 
     //路由配置
     let router = Router::new().route("/", routing::get(index));
 
-    let port: u16 = config::get().server.port();
+    let port: u16 = config::get().server().port();
     // 监听配置
     let bind_add: String = format!("0.0.0.0:{port}");
     let listener: TcpListener = TcpListener::bind(&bind_add).await.unwrap();
 
     tracing::info!("Listener bind {bind_add}");
 
-    axum::serve(listener, router).await.unwrap()
+    axum::serve(listener, router).await.unwrap();
+    Ok(())
 }
 
 #[debug_handler]
